fc4379090d0736796cd85b11d041770ea27645ea
Andre <ameyer@energy-consulting-meyer.de>
2021-07-17 00:14:41 +0200

    rework

--- logging.go
+++ logging.go
@@ -1,146 +1,187 @@
 package logging
 
-// logging package
-// simple python like logging module
-// It's recommended to call BasicConfig first,
-// otherwise the standarts will be used.
-
 import (
 	"fmt"
-	"os"
+	"io"
 	"strings"
 	"time"
 )
 
-// LogLevel int from 0 - 4
+/*
+LOGGING FORMATTING
+	%(TIME) -> time
+	%(LEVEL) -> LogLevel Symbol
+	%(LEVELNAME) -> LogLevel Name
+	%(MSG) -> message
+*/
+
 type LogLevel int
 
-// LogLevel constants
+func (l LogLevel) valid() bool {
+	return l >= 0 && l <= 5
+}
+
+func (l LogLevel) String() string {
+	if !l.valid() {
+		return ""
+	}
+	return []string{"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"}[l]
+}
+
+func (l LogLevel) Symbol() string {
+	if !l.valid() {
+		return ""
+	}
+	return []string{"[+]", "[*]", "[~]", "[!]", "[x]"}[l]
+}
+
+func (l LogLevel) Name() string {
+	return l.String()
+}
+
 const (
-	DEBUG    LogLevel = 0
-	INFO     LogLevel = 1
-	WARNING  LogLevel = 2
-	ERROR    LogLevel = 3
-	CRITICAL LogLevel = 4
+	DEBUG    LogLevel = iota // [+] additional debug information
+	INFO                     // [*] info for the user
+	WARNING                  // [~] unexpected behaviour that might cause problems
+	ERROR                    // [!] error but the program can still run
+	CRITICAL                 // [x] critical error -> program is terminated
 )
 
-// logging prefixes
-const (
-	pDebug    = "[+]"
-	pInfo     = "[*]"
-	pWarning  = "[-]"
-	pError    = "[!]"
-	pCritical = "[!!]"
-)
-
-var (
-	logLevel    LogLevel
-	logFormat   string = "{prefix} {message}"
-	timeFormat  string = "15:24:05"
-	logFile     *os.File
-	printOutput bool
-)
-
-// BasicConfig sets the minimum LogLevel and the format to print
-// it initializes the logging module
-// level can be DEBUG, INFO, WARNING, ERROR, CRITICAL
-// format -> see doc for examples
-// file an *os.File the output will be written to
-// fileOnly whether the output should not be printed but only written to the file
-func BasicConfig(level LogLevel, format string, file *os.File, fileOnly bool) {
-	logLevel = level
-	logFormat = format
-	logFile = file
-	printOutput = (fileOnly == false)
+// Standard Logger
+// contains unexported fields
+type Logger struct {
+	format     string
+	timeformat string
+	writer     io.Writer
+	level      LogLevel
+	nextLine   string
 }
 
-// SetTimeFormat sets a custom time format to output if the logging format contains "{asctime}"
-// the format is the same as go uses in the time package
-func SetTimeFormat(format string) {
-	timeFormat = format
-}
-
-// format the format string appropriately
-// {prefix}
-// {levelname}
-// {asctime}
-// {message}
-func format(prefix, levelname, asctime, message string) string {
-	r := strings.NewReplacer(
-		"{prefix}", prefix,
-		"{levelname}", levelname,
-		"{asctime}", asctime,
-		"{message}", message,
-	)
-	return r.Replace(logFormat)
-}
-
-// Debug logs debug message
-func Debug(a interface{}) {
-	if logLevel == 0 {
-		str := format(pDebug, "DEBUG", time.Now().Format(timeFormat), fmt.Sprintf("%v", a))
-
-		if printOutput || logFile == nil {
-			fmt.Println(str)
-		}
-		if logFile != nil {
-			logFile.WriteString(str)
-		}
+// NewLogger returns a new Logger with format as default format.
+// The format could be eg. "%(TIME): %(MSG)" - %(TIME) will be replaced with the current time
+// and %(MSG) will be replaced with the message
+// writer if the writer is not nil, the message will not only be printed to the stdout but will
+// also be written to the writer (which could be a file)
+// level is the minimum level that the logger will log. If the level is set to INFO logger.Debug
+// will do nothing
+func NewLogger(format string, writer io.Writer, level LogLevel) *Logger {
+	return &Logger{
+		format,
+		"",
+		writer,
+		level,
+		"\n",
 	}
 }
 
-// Info logs info message
-func Info(a interface{}) {
-	if logLevel <= 1 {
-		str := format(pInfo, "INFO", time.Now().Format(timeFormat), fmt.Sprintf("%v", a))
-
-		if printOutput || logFile == nil {
-			fmt.Println(str)
-		}
-		if logFile != nil {
-			logFile.WriteString(str)
-		}
+// Sets the logging level of the Logger
+func (l *Logger) SetLevel(level LogLevel) {
+	if level.valid() {
+		l.level = level
 	}
 }
 
-// Warning logs warning message
-func Warning(a interface{}) {
-	if logLevel <= 2 {
-		str := format(pWarning, "WARNING", time.Now().Format(timeFormat), fmt.Sprintf("%v", a))
-
-		if printOutput || logFile == nil {
-			fmt.Println(str)
-		}
-		if logFile != nil {
-			logFile.WriteString(str)
-		}
-	}
+func (l *Logger) SetWriter(writer io.Writer) {
+	l.writer = writer
 }
 
-// Error logs error message
-func Error(a interface{}) {
-	if logLevel <= 3 {
-		str := format(pError, "ERROR", time.Now().Format(timeFormat), fmt.Sprintf("%v", a))
-
-		if printOutput || logFile == nil {
-			fmt.Println(str)
-		}
-		if logFile != nil {
-			logFile.WriteString(str)
-		}
-	}
+// golangs time Layout eg: "2021-08-18 15:03"
+func (l *Logger) SetTimeFormat(layout string) {
+	l.timeformat = layout
 }
 
-// Critical logs critical message and exits program
-func Critical(a interface{}) {
-	str := format(pCritical, "CRITICAL", time.Now().Format(timeFormat), fmt.Sprintf("%v", a))
+func (l *Logger) SetFormat(format string) {
+	l.format = format
+}
 
-	if printOutput || logFile == nil {
-		fmt.Println(str)
+// SetNextLine is defaulted to a new line at the end of every log call.
+func (l *Logger) SetNextLine(endl string) {
+	l.nextLine = endl
+}
+
+// LOGGING FORMATTING
+// 	   %(TIME) -> time
+// 	   %(LEVEL) -> LogLevel Symbol
+// 	   %(LEVELNAME) -> LogLevel Name
+//     %(MSG) -> message
+func (l *Logger) log(lvl LogLevel, msg string) {
+	if lvl < l.level {
+		return
 	}
-	if logFile != nil {
-		logFile.WriteString(str)
-		logFile.Close()
+
+	m := l.format
+	m += l.nextLine
+	m = strings.ReplaceAll(m, "%(TIME)", time.Now().Format(l.timeformat))
+	m = strings.ReplaceAll(m, "%(LEVEL)", l.level.Symbol())
+	m = strings.ReplaceAll(m, "%(LEVELNAME)", l.level.Name())
+	m = strings.ReplaceAll(m, "%(MSG)", msg)
+
+	if l.writer != nil {
+		l.writer.Write([]byte(m))
 	}
-	os.Exit(1)
+	fmt.Print(m)
+}
+
+func (l *Logger) Log(level LogLevel, msg string) {
+	l.log(level, msg)
+}
+
+func (l *Logger) Debug(msg string) {
+	l.log(DEBUG, msg)
+}
+
+func (l *Logger) Info(msg string) {
+	l.log(INFO, msg)
+}
+
+func (l *Logger) Warning(msg string) {
+	l.log(WARNING, msg)
+}
+
+func (l *Logger) Error(msg string) {
+	l.log(ERROR, msg)
+}
+
+func (l *Logger) Critical(msg string) {
+	l.log(CRITICAL, msg)
+}
+
+var defaultLogger = &Logger{
+	format:     "%(LEVEL) %(TIME): %(MSG)",
+	timeformat: "2021-08-18 15:06:12",
+	writer:     nil,
+	level:      DEBUG,
+	nextLine:   "\n",
+}
+
+func SetLevel(level LogLevel) {
+	defaultLogger.SetLevel(level)
+}
+
+func SetWriter(writer io.Writer) {
+	defaultLogger.SetWriter(writer)
+}
+
+func Log(level LogLevel, msg string) {
+	defaultLogger.Log(level, msg)
+}
+
+func Debug(msg string) {
+	defaultLogger.Debug(msg)
+}
+
+func Info(msg string) {
+	defaultLogger.Info(msg)
+}
+
+func Warning(msg string) {
+	defaultLogger.Warning(msg)
+}
+
+func Error(msg string) {
+	defaultLogger.Error(msg)
+}
+
+func Critical(msg string) {
+	defaultLogger.Critical(msg)
 }
